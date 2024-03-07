c4214a89d7c4b6703b33ad0fd06ad4ec2906c454
honeybee <shelby.e.pagan@gmail.com>
2024-03-07 12:34:42 -0500

    remove portfolio project from projects

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,12 @@
+import React from 'react';
+
+
 function Footer() {
-    return ;
-  }
-  
-  export default Footer;
+  return (
+    <footer className="sticky-footer">
+     <p>&copy; 2024 Shelby Pagan. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
